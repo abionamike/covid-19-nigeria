30da3f50458dd9eacb883a198996a8932e4527f6
Abiona Michael <amabiona21@gmail.com>
2021-05-27 00:43:55 +0100

    working with nigeria covid-19 statistics api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://covidnigeria.herokuapp.com/api')
+      .then(res => res.json())
+      .then(result => setData(result.data))
+  }, []);
+
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <main className="main">
+        <h1 className="title"><div>Covid-19</div> In Nigeria</h1>
+        <div className="description">
+          <p>Total Samples Tested: {data.totalSamplesTested}</p>
+          <p>Total Confirmed Cases: {data.totalConfirmedCases}</p>
+          <p>Total Active Cases: {data.totalActiveCases}</p>
+          <p>Discharged: {data.discharged}</p>
+          <p>Death: {data.death}</p>
+        </div>
+
+        <div className="grid">
+          {data.states.map((state) => (
+            <div key={state._id} className="card">
+              <h2>State: {state.state}</h2>
+              <p>Confirmed cases: {state.confirmedCases}</p>
+              <p>Cases On Admission: {state.casesOnAdmission}</p>
+              <p>Discharged: {state.discharged}</p>
+              <p>Death: {state.death}</p>
+            </div>
+          ))}
+        </div>
+      </main>
+      <footer className="footer">
+        <p>Copyright &copy; {new Date().getFullYear()} DevelopedbyMike</p>
+      </footer>
     </div>
   );
 }
